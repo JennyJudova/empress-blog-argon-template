b7f1872fb0b829afb8c6c51e793bd4740502b170
JennyJudova <jjudova@gmail.com>
2020-05-22 17:09:14 +0100

    feat: postcard to author

--- addon/components/post-card.js
+++ addon/components/post-card.js
@@ -4,10 +4,12 @@ import { htmlSafe } from '@ember/template';
 import layout from '../templates/components/post-card';
 
 export default Component.extend({
+  post: undefined,
   layout,
   tagName: '',
-  computedStyle: computed('post.image', function() {
-    if(!this.post.image) {
+  computedStyle: computed('post.image', 'post', function () {
+    console.log(this.post)
+    if (!this.post.image) {
       return;
     }
 
