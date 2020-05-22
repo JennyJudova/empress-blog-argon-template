f6a6172e33fa825a5e1d9ead2a1074c5673933b5
JennyJudova <jjudova@gmail.com>
2020-05-22 21:00:18 +0100

    fix: fix all lint errors

--- addon/components/post-card.js
+++ addon/components/post-card.js
@@ -8,13 +8,9 @@ export default Component.extend({
   layout,
   tagName: '',
   computedStyle: computed('post.image', 'post', function () {
-    console.log(this.post)
     if (!this.post.image) {
       return;
     }
-
-    // Make sure this is in fact safe!! this does not make things safe, it tells
-    // the template that it is safe.
     return htmlSafe(`background-image: url(${this.post.image})`);
   })
 });
